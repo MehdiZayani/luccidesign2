c3ebfdb1990e6ee8dd99b5d7f22afcae5e3b0431
Badi's Macbook <macbook@Badis-Macbook.local>
2023-12-06 13:27:38 +0100

    v2.1

--- pages/index.js
+++ pages/index.js
@@ -8,6 +8,7 @@ export default function Home() {
   return (
     <div>
       <Head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
         <title>Lucci Design</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
